cf352d880a366c24765835129b6b0c4410f8fd7e
Juan Manuel <juanmanuelgigon@gmail.com>
2025-08-13 19:36:44 -0300

    avances modelos

--- parroquiaproject/sitio/models.py
+++ parroquiaproject/sitio/models.py
@@ -4,6 +4,20 @@ from django.db import models
 class Categoria(models.Model):
     nombre = models.CharField(max_length=50)
 
+class Iglesia(models.Model):
+    nombre = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=100)
+    horarioMisa = models.ForeignKey('HorarioMisa', blank=True, null=True, on_delete=models.CASCADE)
+    listaActividades = models.ManyToManyField('Actividades', blank=True)
+
+class HorarioMisa(models.Model):
+    texto = models.CharField(max_length=1000)
+
+class Actividades(models.Model):
+    titulo = models.CharField(max_length=50)
+    texto = models.CharField(max_length=200)
+    fechaVencimiento = models.DateTimeField()
+
 class Noticia(models.Model):
     titulo = models.CharField(max_length=50)
     texto = models.CharField(max_length=200)
--- parroquiaproject/sitio/views.py
+++ parroquiaproject/sitio/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from sitio.models import HorarioMisa, Iglesia
+from datetime import datetime
+
+
+h = HorarioMisa(texto="Domingos a las 10:00 AM")
+h.save()
+
+i = Iglesia(nombre="Iglesia San Juan", direccion="Calle Falsa 123", horarioMisa=h)
+i.save()
+
 
 # Create your views here.
 def inicio(request):
-    return render(request, 'inicio.html', {})
+    iglesias = Iglesia.objects.all()
+    return render(request, 'inicio.html', {'lista_iglesias': iglesias})
+
+
