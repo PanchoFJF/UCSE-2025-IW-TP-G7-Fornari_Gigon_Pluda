67a9d8452cf04d7c60f0285a6777441753fd9d4f
Juan Manuel <juanmanuelgigon@gmail.com>
2025-08-15 15:39:02 -0300

    cambios login

--- parroquiaproject/parroquiaproject/urls.py
+++ parroquiaproject/parroquiaproject/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from sitio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inicio/', views.inicio, name='inicio'),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),  # rutas login/logout
+    path("accounts/", include("accounts.urls")),  # nuestra ruta signup
 ]
