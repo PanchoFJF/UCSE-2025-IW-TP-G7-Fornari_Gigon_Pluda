76ae241b520bf70e81c5ce16caca31bd83518286
Juan Manuel <juanmanuelgigon@gmail.com>
2025-09-01 01:43:15 -0300

    cambios email 2

--- parroquiaproject/parroquiaproject/settings.py
+++ parroquiaproject/parroquiaproject/settings.py
@@ -154,10 +154,12 @@ MESSAGE_TAGS = {
     messages.ERROR: "error",
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # o tu proveedor
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'parroquiasingweb@gmail.com'
-EMAIL_HOST_PASSWORD = '1989Gigon'
-DEFAULT_FROM_EMAIL = 'parroquiasingweb@gmail.com'
+import os
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True") == "True"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
