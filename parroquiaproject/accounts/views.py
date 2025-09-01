d402eeea9059f748187656315e60600ac7cb9032
Juan Manuel <juanmanuelgigon@gmail.com>
2025-09-01 01:27:47 -0300

    cambios email

--- parroquiaproject/accounts/views.py
+++ parroquiaproject/accounts/views.py
@@ -1,21 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-from django.contrib.auth.tokens import default_token_generator
-from django.contrib.auth import login as auth_login
-from django.shortcuts import render, redirect
-from django.urls import reverse, reverse_lazy
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.utils.encoding import force_bytes
-from django.contrib.auth.models import User
-from django.http import HttpResponse
 from django.contrib import messages
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.views import LogoutView
-from django.shortcuts import render, redirect
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
@@ -24,61 +10,52 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
 from django.conf import settings
+from django.contrib.auth.views import LoginView, LogoutView
 
 from .forms import SignUpForm
 
-# from django.core.mail import send_mail
-
-
+# --- Registro con confirmación por email ---
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")  # después de registrarse va al login
+    form_class = SignUpForm
     template_name = "registration/signup.html"
+    success_url = reverse_lazy("login")  # redirige al login tras enviar email
 
     def form_valid(self, form):
-        messages.success(self.request, "¡Tu cuenta fue creada con éxito!")
+        user = form.save(commit=False)
+        user.is_active = False  # 👈 queda inactivo hasta confirmar
+        user.save()
+
+        # enviar email de activación
+        current_site = get_current_site(self.request)
+        subject = "Activa tu cuenta"
+        message = render_to_string("registration/activation_email.html", {
+            "user": user,
+            "domain": current_site.domain,
+            "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+            "token": default_token_generator.make_token(user),
+        })
+        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user.email])
+
+        messages.success(self.request, "¡Cuenta creada! Revisa tu correo para activarla.")
         return super().form_valid(form)
-    
+
+# --- Login y Logout personalizados ---
 class CustomLoginView(LoginView):
     template_name = "registration/login.html"
 
     def get_success_url(self):
-        username = self.request.user.username  # <- acá obtenemos el username
+        username = self.request.user.username
         messages.success(self.request, f"¡Bienvenido de nuevo {username}!")
         return super().get_success_url()
-    
+
 class CustomLogoutView(LogoutView):
     next_page = "inicio"
 
     def dispatch(self, request, *args, **kwargs):
         messages.success(request, "Sesión cerrada correctamente.")
         return super().dispatch(request, *args, **kwargs)
-    
-def signup(request):
-    if request.method == "POST":
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.is_active = False  # 👈 queda inactivo hasta confirmar
-            user.save()
 
-            # datos para el mail
-            current_site = get_current_site(request)
-            subject = "Activa tu cuenta"
-            message = render_to_string("registration/activation_email.html", {
-                "user": user,
-                "domain": current_site.domain,
-                "uid": urlsafe_base64_encode(force_bytes(user.pk)),
-                "token": default_token_generator.make_token(user),
-            })
-            send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user.email])
-
-            return render(request, "registration/activation_sent.html")
-    else:
-        form = SignUpForm()
-    return render(request, "registration/signup.html", {"form": form})
-
-# views.py
+# --- Activación de usuario ---
 def activate(request, uidb64, token):
     try:
         uid = force_str(urlsafe_base64_decode(uidb64))
@@ -89,6 +66,7 @@ def activate(request, uidb64, token):
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
+        messages.success(request, "¡Cuenta activada correctamente! Ya podés iniciar sesión.")
         return render(request, "registration/activation_success.html")
     else:
         return render(request, "registration/activation_invalid.html")
