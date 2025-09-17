13e2f4830ddc1bae59b903426697691e80f71a3f
Juan Manuel <juanmanuelgigon@gmail.com>
2025-09-17 20:38:12 -0300

    cambios modelos

--- parroquiaproject/sitio/models.py
+++ parroquiaproject/sitio/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime, timedelta
+from django.utils.timezone import now
 
 # Create your models here.
+def fecha_vencimiento(dias):
+    return now() + timedelta(days=dias) 
 
 class Iglesia(models.Model):
     nombre = models.CharField(max_length=50)
@@ -17,7 +21,10 @@ class Noticia(models.Model):
     descripcion = models.TextField()
     imagen = models.ImageField(upload_to="inicio/", blank=True, null=True)
     fecha = models.DateTimeField(auto_now_add=True)
-
+    iglesiaAsociada = models.ForeignKey(Iglesia, blank=True, null=True, on_delete=models.CASCADE)
+    estado = models.BooleanField(default=False)  # True = visible, False = archivada
+    fechaVencimiento = models.DateTimeField(null=True, default=fecha_vencimiento(30))
+    fechaAceptacion = models.DateTimeField(null=True, default=fecha_vencimiento(7))
     def __str__(self):
         return self.titulo
  
@@ -29,7 +36,6 @@ class Actividades(models.Model):
     titulo = models.CharField(max_length=50)
     texto = models.CharField(max_length=200)
     fechaVencimiento = models.DateTimeField(null=True)
-    
     def __str__(self):
         return self.titulo
 
@@ -40,4 +46,8 @@ class UsuarioIglesias(models.Model):
 
     def __str__(self):  
         return self.usuario.username
+    
+
+
+
     
