1da0e887c556dd2a6b13441555b0f7199f174c84
Juan Manuel <juanmanuelgigon@gmail.com>
2025-09-19 18:07:08 -0300

    cambios estado

--- parroquiaproject/sitio/models.py
+++ parroquiaproject/sitio/models.py
@@ -23,7 +23,7 @@ class Noticia(models.Model):
     fecha = models.DateTimeField(auto_now_add=True)
     iglesiaAsociada = models.ForeignKey(Iglesia, blank=True, null=True, on_delete=models.CASCADE)
     creador = models.ForeignKey(User, on_delete=models.CASCADE)
-    estado = models.BooleanField(default=False)  # True = visible, False = archivada
+    estado = models.BooleanField(null=True, default=None)  # True = visible, False = archivada
     fechaVencimiento = models.DateTimeField(null=True, default=fecha_vencimiento(30))
     fechaAceptacion = models.DateTimeField(null=True, default=fecha_vencimiento(7))
     def __str__(self):
