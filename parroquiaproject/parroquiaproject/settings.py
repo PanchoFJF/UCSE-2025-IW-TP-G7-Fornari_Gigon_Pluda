eaaad5f071303a9f375858627fcf270227b33930
Pancho <fornaripancho@gmail.com>
2025-10-01 01:58:31 -0300

    Update settings (correo outlook)

--- parroquiaproject/parroquiaproject/settings.py
+++ parroquiaproject/parroquiaproject/settings.py
@@ -178,7 +178,7 @@ import os
     #DEFAULT_FROM_EMAIL = "test@example.com"
 #else:
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp.office365.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 #EMAIL_USE_SSL = False
